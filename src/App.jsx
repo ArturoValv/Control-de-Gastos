3d125828a379f00d7a4811af0c82749158bd8b78
Arturo Valverde <art.valverde.563@gmail.com>
2022-09-28 00:37:45 -0500

    Se agregó componente Modal y  botones para abrir y cerrar modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import Modal from "./components/Modal";
 import iconoNuevoGasto from "./img/nuevo-gasto.svg";
 
 function App() {
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+
+  const [modal, setModal] = useState(false);
+
+  const handleNuevoGasto = () => {
+    setModal(true);
+  };
 
   return (
     <div>
@@ -17,9 +24,15 @@ function App() {
 
       {isValidPresupuesto && (
         <div className="nuevo-gasto">
-          <img src={iconoNuevoGasto} alt="icono nuevo gasto" />
+          <img
+            src={iconoNuevoGasto}
+            alt="icono nuevo gasto"
+            onClick={handleNuevoGasto}
+          />
         </div>
       )}
+
+      {modal && <Modal setModal={setModal} />}
     </div>
   );
 }
