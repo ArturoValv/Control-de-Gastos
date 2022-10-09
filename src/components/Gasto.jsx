7d5ceef1b21699437b2a945e01c9fd0f8e131f55
Arturo Valverde <art.valverde.563@gmail.com>
2022-10-09 02:06:45 -0500

    se le dió formato a la fecha y se imprimió en la lista

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { formatearFecha } from "../helpers";
 
 const Gasto = ({ gasto }) => {
-  const { categoria, concepto, monto, id } = gasto;
+  const { categoria, concepto, monto, id, fecha } = gasto;
   return (
     <div className="gasto sombra">
       <div className="contenido-gasto">
+        {/*Imagen*/}
         <div className="descripcion-gasto">
           <p className="categoria">{categoria}</p>
           <p className="nombre-gasto">{concepto}</p>
+          <p className="fecha-gasto">
+            Agregado el: {""}
+            <span>{formatearFecha(fecha)}</span>
+          </p>
         </div>
       </div>
+      <p className="cantidad-gasto">${monto}</p>
     </div>
   );
 };
